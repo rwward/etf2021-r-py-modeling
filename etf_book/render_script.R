a7aaef291cfea4448cdccabd9df30e3d8199ef23
dusty-turner <dusty.s.turner.mil@mail.mil>
2021-10-18 09:02:32 -0400

    need to pull

--- etf_book/render_script.R
+++ etf_book/render_script.R
@@ -1,6 +1,8 @@
 library(bookdown)
 # bookdown::render_book("Intermediate_R.Rmd", "bookdown::gitbook",output_dir = "MORS_ETF_2020",clean_envir = TRUE)
+
 bookdown::render_book(input = "index.Rmd", "bookdown::gitbook",output_dir = "MORS_ETF_2021")
+
 # serve_book(dir = ".", output_dir = "_book", preview = TRUE, in_session = TRUE, quiet = FALSE)
 # rmarkdown::render_site(encoding = 'UTF-8') #HTML and PDF
 
